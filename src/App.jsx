23dcb814eb1f608c69d24578535250fc393fbd60
Fahrul Ahyan <fahyandp@gmail.com>
2023-08-27 05:05:01 +0700

    tidying up

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ import {
   ModalFooter,
   ModalBody,
   useDisclosure,
-  Button
+  Button,
+  Text,
 } from "@chakra-ui/react";
 import { HamburgerIcon, InfoOutlineIcon } from "@chakra-ui/icons";
 import { NotesContainer } from "./components/notes_container";
@@ -32,22 +33,23 @@ const colors = {
 
 function App() {
   // Chakra UI Hook for Modal
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
+  // Hook to show the Introduction Modal
   useEffect(() => {
     if (localStorage.getItem("status")) {
-      console.log("welcome back")
+      console.log("welcome back");
     } else {
-      localStorage.setItem("status", "true")
-      onOpen()
+      localStorage.setItem("status", "true");
+      onOpen();
     }
-  }, [])
+  }, []);
 
   // Key Binding Functions for the Input Component
   useEffect(() => {
     function handleKeyDown(event) {
       if (event.altKey && event.key === "i") {
-        onOpen()
+        onOpen();
         console.log("Button was pressed");
         event.preventDefault();
       }
@@ -66,12 +68,19 @@ function App() {
         <ModalContent>
           <ModalHeader textAlign={"center"}>Welcome to QuickWrite</ModalHeader>
           <ModalBody textAlign={"justify"}>
-            QuickWrite is a simple note taking app for you to easily put your spontanious Ideas.
-            <br />
-            Long gone the day of having to find a note app, open it, start a new note, and save it.
-            With QuickWrite, you can just write a note, add, and go about your day.
-            <br />
-            We believe all ideas matters, even if it's brief. So write them, QuickWrite them.
+            <Text letterSpacing={"wide"} lineHeight={"7"}>
+              <Text as="b">QuickWrite</Text> is a simple note-taking app for
+              easily jotting down your <Text as="em">spontaneous ideas</Text>.
+              <br />
+              No more searching for a note app, opening it, starting a new note,
+              and saving it - with <Text as="b">QuickWrite</Text>, you can
+              simply write a note, add it, and continue with your day.
+              <br />
+              We believe that all ideas matter, even if they are{" "}
+              <Text as="em">brief</Text>.
+              <br />
+              So write them, and <Text as="b">QuickWrite</Text> them.
+            </Text>
           </ModalBody>
 
           <ModalFooter>
@@ -84,14 +93,14 @@ function App() {
       <Grid
         templateAreas={`"header"
         "main"`}
-        gridTemplateRows="10vh 1fr"
+        gridTemplateRows="12vh 1fr"
         h="100vh"
         gap="0"
         color="blackAlpha.700"
         fontWeight="bold"
         textColor={colors["pallete-5"]}
       >
-        <GridItem bg={colors["pallete-2"]} area="header">
+        <GridItem bg={colors["pallete-2"]} area="header" >
           <HStack spacing="1.5em" p={2}>
             <Menu>
               <MenuButton
